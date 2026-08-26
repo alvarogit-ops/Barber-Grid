fd4b66e576faf5ccd2fbf1115f6213516effcaef
Alvaro Yuri <alvaro.yuri@outlook.com>
2026-08-26 08:46:10 -0300

    Implementando views de autenticação

--- barber_grid/urls.py
+++ barber_grid/urls.py
@@ -4,6 +4,7 @@ from . import views
 #Define uma lista de url patterns
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('accounts/login', views.login) #aponta para uma função que existe em views. 
 ]
 
