e4f7b73da3f37014c378fb2c5e784e4f6c02ff1f
Alvaro Yuri <alvaro.yuri@outlook.com>
2026-08-28 09:54:18 -0300

    Cria login e registro closes #4

--- barber_grid/urls.py
+++ barber_grid/urls.py
@@ -4,7 +4,9 @@ from . import views
 #Define uma lista de url patterns
 
 urlpatterns = [
-    path('', views.index),
-    path('accounts/login', views.login) #aponta para uma função que existe em views. 
+    path('', views.index, name="index"),
+    path('login/', views.login, name='login'), #aponta para uma função que existe em views. 
+    path('registro', views.registro, name='registro'),
+    path('painel_admin', views.painel_admin, name="painel_admin")
 ]
 
--- barber_grid/views.py
+++ barber_grid/views.py
@@ -1,35 +1,57 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth import authenticate, login as auth_login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ValidationError
-
-def loginForm(request):
+from django.core.exceptions import PermissionDenied
+from .models import Agendamento
+def login(request):
     # Verifico se um usuário existe e se a senha está correta
 
     if request.method == 'POST':
-        nome_usuario = request.POST['nome_usuario']
+        email = request.POST['email']
         senha = request.POST['senha']
-        user = authenticate(request, username = nome_usuario, password=senha)
 
-    if user is not None:
-        login(request, user)
-        # redirecionar o usuário para próxima página
-    else:
-        raise ValidationError("Credenciais inválidas")
+        ## Passar um usario obtendo email
 
-    return render(request, 'accounts/login.html')
+        try:
+            usuario = User.objects.get(email=email)
 
-def RegisterForm(request):
+        except User.DoesNotExist:
+            raise ValidationError('Credenciais inválidas!')
+
+        user = authenticate(request, username=usuario.username, password=senha)
+
+        # identação corrigia: user não era acessado
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index')
+        # redirecionar o usuário para próxima página e com auth_login
+        else:
+            raise ValidationError("Credenciais inválidas")
+
+    return render(request, 'barber_grid/login.html') #apontar um app para um template
+
+def registro(request):
     #Aqui eu crio um usuário, os valores vem de uma requisição POST
 
     if request.method == 'POST':
         nome_usuario = request.POST['nome_usuario']
+        email = request.POST['email']
         senha = request.POST['senha']
-        user = User.objects.create_user(username = nome_usuario , password = senha)
+        telefone = request.POST['telefone']
+        confirmar_senha = request.POST['confirmar_senha']
 
+        if senha != confirmar_senha:
+            raise ValidationError("As senhas não são iguais!")
 
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Este e-mail já está cadastrado!')
+
+        user = User.objects.create_user(username = nome_usuario , email=email, password = senha)
+
+    return render(request, 'barber_grid/registro.html')
     #Geralmente no navegador nós acessamos com a requisição GET
 
 def logoutForm(request):
@@ -37,8 +59,6 @@ def logoutForm(request):
 
 @login_required
 def index(request):
-    login_url = '/login/'
-    redirect = ''
     return render(request, 'barber_grid/home.html')
 
 class PerfilProtegido(LoginRequiredMixin):
@@ -53,3 +73,18 @@ def password_change(request):
             update_session_auth_hash(request, form.user)
      else:
         pass
+
+@login_required
+def painel_admin(request):
+
+    if not request.user.is_staff:
+        raise PermissionDenied
+
+    agendamentos = Agendamento.objects.all()
+
+    return render(
+        request,
+        'barber_grid/agendamentos.html',
+        {'agendamentos': agendamentos}
+    )
+
